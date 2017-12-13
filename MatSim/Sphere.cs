694d28e4b785907762fceff804b8dc531e9b6613
Sanngrithr <34239794+Sanngrithr@users.noreply.github.com>
2017-12-13 15:33:56 +0100

    Add files via upload

---
+++ MatSim/Sphere.cs
@@ -0,0 +1,26 @@
+using Fusee.Math.Core;
+
+public class Sphere
+{
+    private float coordinateX(int t, int p){
+
+        float vecx = (M.Cos(t) * M.Sin(p));
+
+        return vecx;
+    }
+
+    private float coordinateY(int t, int p){
+
+        float vecy =(M.Cos(t) * M.Sin(p));
+
+        return vecy;
+    }
+
+    private float coordinateZ(int t, int p){
+
+        float vecz = (M.Sin(t));
+
+        return vecz;
+    }
+
+}
---
+++ MatSim/SphereLocation.cs
@@ -0,0 +1,8 @@
+using Fusee.Math.Core;
+public class SphereLocation{
+
+    public SphereLocation(float x, float y, float z){
+
+        float3 spherevector= (x, y, z);
+    }
+}
